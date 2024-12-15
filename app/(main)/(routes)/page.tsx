de85dfb5601d4359f14d2159bb9bf93cf5fe45ee
111ximo <120021560+111ximo@users.noreply.github.com>
2024-12-15 23:06:12 +0800

    登录界面彻底完成

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,12 +1,27 @@
-// import {Button} from "@/components/ui/button";
-// import{cn} from "@/lib/utils";
-// const state=true;
+"use client"; // Mark the component as client-side
+
+import { UserButton, useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation"; // Use next/navigation for routing
+import { useEffect } from "react";
 
 export default function Home() {
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isSignedIn) {
+      router.push("/sign-in");
+    }
+  }, [isSignedIn, router]);
+
   return (
     <div>
-        This is a protected route.
+      {/* {isSignedIn ? (
+        <div>Welcome back! You are signed in.</div>
+      ) : (
+        <div>Redirecting to sign-in...</div>
+      )} */}
+      <UserButton afterSwitchSessionUrl="/" />
     </div>
-    
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware,createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']); // 公开路由
 
-export default clerkMiddleware(async (auth, request) => {
-  if (!isPublicRoute(request)) {
-    await auth.protect()
-  }
-})
+export default clerkMiddleware(
+  // async (auth, request) => {
+  // // 如果访问的是需要保护的路由，才要求认证
+  // if (!isPublicRoute(request)) {
+  //   await auth.protect();  // 如果用户没有认证，保护路由
+  //   console.log(auth)
+  // }}
+);
 
+// export default clerkMiddleware({})
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    '/(api|trpc)(.*)', // 始终保护 API 路由
   ],
-}
+};
