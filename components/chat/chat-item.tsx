973caf5df9751d38865393f992848d29bd5b2298
111ximo <120021560+111ximo@users.noreply.github.com>
2025-01-10 16:56:08 +0800

    完成了trash的功能

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -21,6 +21,7 @@ import{
 }from "@/components/ui/form"
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatItemProps{
     id:string;
@@ -60,7 +61,7 @@ export const ChatItem=({
     socketQuery
 }:ChatItemProps)=>{
     const [isEditing,setIsEditing]=useState(false);
-    const [isDeleting,setIsDeleting]=useState(false);
+    const {onOpen}=useModal();
 
     useEffect(()=>{
         const handleKeyDown=(event:any)=>{
@@ -90,6 +91,9 @@ export const ChatItem=({
             })
 
             await axios.patch(url,values);
+
+            form.reset();
+            setIsEditing(false);
         }catch(error){
             console.error(error);
         }
@@ -227,6 +231,10 @@ export const ChatItem=({
                     )}
                     <ActionTooltip label="Edit">
                         <Trash 
+                            onClick={()=>onOpen("deleteMessage",{
+                                apiUrl:`${socketUrl}/${id}`,
+                                query:socketQuery
+                            })}
                             className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
                         />
                     </ActionTooltip>
