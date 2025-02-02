4059082590940b231430956a724d0118cfa1c24d
111ximo <120021560+111ximo@users.noreply.github.com>
2025-02-02 16:51:38 +0800

    将width的的变化改成了transform，这样就可以避免回流和重绘，提高性能。

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,11 +3,10 @@ import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 const MainLayout = async ({children}:{children:React.ReactNode}) => {
     return ( 
         <div className="h-full">
-            <div className="w-0 md:w-[72px] overflow-hidden z-30 flex-col fixed inset-y-0">
-            
+            <div className="transition-transform duration-300 transform md:translate-x-0 -translate-x-full fixed inset-y-0 z-30 flex-col">
                 <NavigationSidebar />
             </div>
-            <main className="pl-0 md:pl-[72px] h-full">
+            <main className="transition-all duration-300 pl-0 md:pl-[72px] h-full">
                 {children}
             </main>
         </div>
