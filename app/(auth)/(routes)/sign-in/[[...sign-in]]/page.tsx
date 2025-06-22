8a68ceac38127df025714dbf08f29f00cd52b524
111ximo <120021560+111ximo@users.noreply.github.com>
2025-06-23 03:55:18 +0800

    fix: 修改bug

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 // export default SignInPage;
-import { SignUp } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return <SignIn />
 }
